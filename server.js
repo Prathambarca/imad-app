51899f313d8ae3d6bc7694832730aadb77be5c66
Prathambarca <pratham.shirshivkar@gmail.com>
2018-03-02 17:21:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
   });
 
-
-
 app.get('/article', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article.html'));
 });
