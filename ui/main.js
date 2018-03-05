db784dc6c0af6dfe8e2c48a2bf48f872b0e3d257
PRATHMESH SHIRSHIVKAR <pratham.shirshivkar@gmail.com>
2018-03-05 21:31:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,11 @@
-console.log('Loaded!');
-var img=document.getElementById('madi');
-img.onclick = function(){
-    img.style.marginLeft ='100px';
+var button=document.getElementById('counter');
+button.onclick = function() {
+    counter=counter+1;
+var span=document.getElementById('count');
+span.innerHTML=counter.toString();
+    
+    
 };
+
+
+    
